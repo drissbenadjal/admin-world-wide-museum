e85252e52d22069680d489121fc0a0060ddac9cb
DRISS BENADJAL <driss.benadjal@edu.univ-eiffel.fr>
2023-03-06 17:06:26 +0100

    Update StatsResa.jsx

--- src/components/StatsResa.jsx
+++ src/components/StatsResa.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 export const StatsResa = ({ reservations }) => {
+
     const [reservationsThisWeek, setReservationsThisWeek] = useState([]);
+    const [reservationsLastWeek, setReservationsLastWeek] = useState([]);
+    const [pourcentage, setPourcentage] = useState(0);
 
     useEffect(() => {
         if (reservations.length === 0) {
@@ -21,15 +24,43 @@ export const StatsResa = ({ reservations }) => {
                 setReservationsThisWeek((reservationsThisWeek) => [...reservationsThisWeek, resa]);
             }
         });
+
+        const lastMonday = new Date(now.setDate(now.getDate() - now.getDay() - 6));
+        const lastSunday = new Date(now.setDate(now.getDate() - now.getDay() + 0));
+
+        reservations.filter((resa) => {
+            const resaDate = new Date(resa.date_reservation);
+            if (resaDate >= lastMonday && resaDate <= lastSunday) {
+                setReservationsLastWeek((reservationsLastWeek) => [...reservationsLastWeek, resa]);
+            }
+        });
     }, [reservations]);
 
+    useEffect(() => {
+        //calculer le pourcentage d'augmentation ou de diminution des réservations
+        const pourcentage = Math.round((reservationsThisWeek.length - reservationsLastWeek.length) / reservationsLastWeek.length * 100);
+        //si c'est positif, on affiche un "+" devant le pourcentage et si c'est négatif, on affiche un "-" devant le pourcentage
+        //si c'est NaN ou infinty on met 100%
+        if (isNaN(pourcentage) || !isFinite(pourcentage)) {
+            setPourcentage("+ " + 100);
+        } else {
+            if (pourcentage > 0) {
+                setPourcentage("+ " + pourcentage);
+            } else {
+                setPourcentage(pourcentage);
+            }
+        }
+    }, [reservationsThisWeek, reservationsLastWeek]);
 
-    console.log(reservationsThisWeek);
 
     return (
         <section className="statCards">
             <div className="statCard">
-                <span className="statCard__progression">+ 20%</span>
+                <span className="statCard__progression">
+                    {
+                        pourcentage + "%"
+                    }
+                </span>
                 <div className="statCard__number">{reservationsThisWeek.length}</div>
                 <div className="statCard__label">Réservations cette semaine</div>
             </div>
@@ -40,7 +71,7 @@ export const StatsResa = ({ reservations }) => {
             <div className="statCard">
                 <span className="statCard__progression">+ 20%</span>
                 <div className="statCard__number">354</div>
-                <div className="statCard__label">Visiteurs</div>
+                <div className="statCard__label">Visiteurs cette semaine</div>
             </div>
         </section>
     );
