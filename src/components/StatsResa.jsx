a93caf953ecb0fe2d83baa84c8d60384b9deb808
DRISS BENADJAL <driss.benadjal@edu.univ-eiffel.fr>
2023-03-09 09:07:01 +0100

    add visiteurs stats

--- src/components/StatsResa.jsx
+++ src/components/StatsResa.jsx
@@ -1,10 +1,71 @@
 import React, { useState, useEffect } from "react";
+import { getCookie } from "../utils/cookieUtils";
 
 export const StatsResa = ({ reservations }) => {
   const resa = reservations;
   const [reservationsThisWeek, setReservationsThisWeek] = useState([]);
   const [reservationsLastWeek, setReservationsLastWeek] = useState([]);
   const [pourcentage, setPourcentage] = useState(0);
+  const [visiteurs, setVisiteurs] = useState([]);
+  const [visiteursThisWeek, setVisiteursThisWeek] = useState([]);
+  const [visiteursNbThisWeek, setVisiteursNbThisWeek] = useState(0);
+
+  useEffect(() => {
+    fetch('https://benadjal.butmmi.o2switch.site/api_resa_expo/visiteurs/',
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: getCookie("token"),
+        }),
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        setVisiteurs(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    //recuperer les "place_reservation" dans visiteurs de cette semaine par rapport a "date_reservation"
+    if (visiteurs.length === 0) {
+      return;
+    }
+    const now = new Date();
+    // obtenir le lundi de la semaine en cours
+    const monday = new Date(now.setDate(now.getDate() - now.getDay() + 1));
+    // obtenir le dimanche de la semaine en cours
+    const sunday = new Date(now.setDate(now.getDate() - now.getDay() + 7));
+
+    visiteurs.filter((v) => {
+      const visiteurDate = new Date(v.date_reservation);
+      //si c'est cette semaine prendre prendre les places et les additionner
+      if (visiteurDate >= monday && visiteurDate <= sunday) {
+        setVisiteursThisWeek((visiteursThisWeek) => [
+          ...visiteursThisWeek,
+          v,
+        ]);
+      }
+    });
+  }, [visiteurs]);
+
+  useEffect(() => {
+    //additionner les places de cette semaine
+    if (visiteursThisWeek.length === 0) {
+      return;
+    }
+    let total = 0;
+    visiteursThisWeek.forEach((v) => {
+      total = total + parseInt(v.place_reservation);
+    });
+    setVisiteursNbThisWeek(total);
+  }, [visiteursThisWeek]);
+
+
 
   useEffect(() => {
     if (resa.length === 0) {
@@ -84,7 +145,9 @@ export const StatsResa = ({ reservations }) => {
         <div className="statCard__label">Total de réservations</div>
       </div>
       <div className="statCard">
-        <div className="statCard__number">354</div>
+        <div className="statCard__number">
+          {visiteursNbThisWeek}
+        </div>
         <div className="statCard__label">Visiteurs cette semaine</div>
       </div>
     </section>
